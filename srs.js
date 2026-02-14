7b331fe11431a9ecb09ee69f148987ab43aa429c
SAHIDUL-ISLAM <sahidul99622@gmail.com>
2026-02-15 00:26:28 +0600

    getElementByClassName

--- srs.js
+++ srs.js
@@ -1,2 +1,5 @@
 const idSelector = document.getElementById('headingOne');
 idSelector.style.backgroundColor = "pink";
+
+const classSele = document.getElementsByClassName("classSelector");
+classSele[0].style.backgroundColor = "yellow";
