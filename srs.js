e79090faef281f432e111ffce08fdb6ec2b82539
sahidul-islam <sahidul99622@gmail.com>
2026-02-15 09:47:18 +0600

    add getElementsByTagName

--- srs.js
+++ srs.js
@@ -1,5 +1,17 @@
+// getElementById
 const idSelector = document.getElementById('headingOne');
 idSelector.style.backgroundColor = "pink";
+// getElementById
 
+// getElementsByClassName
 const classSele = document.getElementsByClassName("classSelector");
 classSele[0].style.backgroundColor = "yellow";
+// getElementsByClassName
+
+// getElementsByTagName
+// output in the console 
+const tagnames = document.getElementsByTagName('li');
+for(const tagname of tagnames ){
+    console.log(tagname.innerText);
+}
+// getElementsByTagName
